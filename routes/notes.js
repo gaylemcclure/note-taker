6e6b9a56e1092103a11584bb3c2f60ea00267f55
Gayle McClure <gaylekdennison@gmail.com>
2024-05-07 16:04:02 +1000

    updated note.js and index.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,34 @@
 const note = require('express').Router();
+const fs = require("fs");
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the feedback
 note.get('/', (req, res) =>
-  readFromFile('').then((data) => res.json(JSON.parse(data)))
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+
 );
 
 // POST Route for submitting feedback
 note.post('/', (req, res) => {
   // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
+  const { title, text, id } = req.body;
 
   // If all the required properties are present
-  if (title && text) {
+  if (title && text && id) {
     // Variable for the object we will save
     const newNote = {
       title,
-      text
+      text,
+      id
     };
 
-    readAndAppend(newNote, "../db/db.json");
+    readAndAppend(newNote, "./db/db.json");
 
     const response = {
       status: 'success',
       body: newNote,
     };
-
     res.json(response);
   } else {
     res.json('Error in posting feedback');
